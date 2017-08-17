e6396596b253a0c38a4f18017434dc486530bc92
Bo Liu <bo-rc@acm.org>
2017-08-16 21:35:18 -0500

    Create connect-test.py

---
+++ connect-test.py
@@ -0,0 +1,17 @@
+#!~/miniconda3/envs/dronekit/bin/python
+
+import time
+from dronekit import connect, VehicleMode
+
+# vehicle = connect("tcp:127.0.0.1:5760", wait_ready=True) # TCP
+# vehicle = connect("127.0.0.1:14550", wait_ready=True) # UDP
+vehicle = connect("/dev/ttyMFD1", wait_ready=True) # serial to pixhawk2
+
+print(" Mode: " + vehicle.mode.name)
+print(" GPS: " + vehicle.gps_0)
+print(" Global Location: " + vehicle.location.global_frame)
+print(" Local Location: " + vehicle.location.local_frame)
+
+vehicle.close()
+
+print("Completed")
