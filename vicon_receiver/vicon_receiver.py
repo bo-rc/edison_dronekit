e6c4a113f9858418fe27a345e37941bfb50297ac
Bo Liu <bo.robotics@gmail.com>
2017-09-08 03:13:00 +0000

    add test for reciever code

--- vicon_receiver/vicon_receiver.py
+++ vicon_receiver/vicon_receiver.py
@@ -1,12 +1,24 @@
 from dronekit import connect, VehicleMode
+import socket
 
 import time
 from pymavlink import mavutil
 
-data = mavutil.mavlink_connection('udp:192.168.1.11:45454')
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+ip = get_ip_address()
+print(ip)
+port = '45455'
+connection_string = 'udp:' + ip + ':' + port
 
+data = mavutil.mavlink_connection(connection_string)
+counter = 0
 while True:
     msg = data.recv_match(type='LOCAL_POSITION_NED')
     # If we have a valid message
     if msg is not None:
         print(msg.x, msg.y, msg.z, msg.vx, msg.vy, msg.vz)
+	print(counter)
+	counter += 1
